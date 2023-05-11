102efaa61341f3339186f09eec3e4d17f78179c9
Osman Siqueira Jr <osman.siqueira@tjpe.jus.br>
2023-05-11 19:27:05 -0300

    [commit 006] - Codigo da aula 5.10: Extarnalizando a consulta JPQL num arquivo xml

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,8 +15,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxafinal );
 	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
-	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome );
 	List<Restaurante> findTop2ByNomeContaining(String nome);
