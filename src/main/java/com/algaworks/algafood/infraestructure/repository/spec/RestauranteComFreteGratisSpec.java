5940aadb9a46343327155c413af0a741dc94ed06
Osman Siqueira Jr <osman.siqueira@tjpe.jus.br>
2023-05-16 19:29:20 -0300

    [commit 012] - Codigo da aula 5.16 e 5.17: usando specifications

---
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/spec/RestauranteComFreteGratisSpec.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.infraestructure.repository.spec;
+
+import java.math.BigDecimal;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class RestauranteComFreteGratisSpec implements Specification {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder builder) {
+		return builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+	}
+
+}
