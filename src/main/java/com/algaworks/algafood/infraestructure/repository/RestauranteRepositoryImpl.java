b7f1588c2b96096e7ed91f104e9c452fca3e9522
Osman Siqueira Jr <osman.siqueira@tjpe.jus.br>
2023-05-18 16:17:28 -0300

    [commit 014] - Codigo da aula 5.19: Melhorando o método explicando a anotação @Lazy

--- src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.infraestructure.repository;
 
+import static com.algaworks.algafood.infraestructure.repository.spec.RestauranteSpecs.comFreteGratis;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,22 +9,27 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
+import com.algaworks.algafood.infraestructure.repository.spec.RestauranteSpecs;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	@Lazy
+	private RestauranteRepository restauranteRepository ;
 	
 	@Override
 	public List<Restaurante> find (String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
@@ -51,6 +58,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		return query.getResultList();
 		
 
+	}
+
+	@Override
+	public List<Restaurante> findComFreteGratis(String nome) {
+		
+		return restauranteRepository.findAll(comFreteGratis().and(RestauranteSpecs.comNomeSemelhante(nome)));
 	}
 
 
