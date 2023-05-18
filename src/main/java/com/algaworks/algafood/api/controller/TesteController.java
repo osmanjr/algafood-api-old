778f6dec399d21752ecc87a21471c25e723610d2
Osman Siqueira Jr <osman.siqueira@tjpe.jus.br>
2023-05-18 17:58:08 -0300

    [commit 015] - Codigo da aula 5.20: Criando um repositório customizado herdando do repositório padrão do JPA, para fazer com que um método seja herdado por padrão (adicionando no repositório base do spring) para todo mundo

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -42,6 +42,12 @@ public class TesteController {
 		return cozinhaRepository.existsByNome(nome);
 	}
 	
+	@GetMapping("/cozinhas/primeira")
+	public Optional<Cozinha> cozinhaPrimeira() {
+		return cozinhaRepository.buscarPrimeiro();
+	}
+	
+	
 	@GetMapping("/restaurantes/por-taxa-frete")
 	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
 		return restauranteRepository.queryByTaxaFreteBetween(taxaInicial, taxaFinal);
@@ -74,11 +80,14 @@ public class TesteController {
 	
 	@GetMapping("/restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome) {
-	
 		return restauranteRepository.findComFreteGratis(nome);
-		
-
 	}
+	
+	@GetMapping("/restaurantes/primeiro")
+	public Optional<Restaurante> restaurantePrimeiro() {
+		return restauranteRepository.buscarPrimeiro();
+	}
+	
 	
 	
 	
