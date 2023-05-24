1964c05b9a05567618d7e56c08b53de64090ca44
osman.siqueira <osman.siqueira@gmail.com>
2023-05-24 18:39:24 -0300

    [commit 031] - Codigo da Aula 7.6: Adicionando flyway no projeto e fazendo primeira migracao

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -30,14 +30,4 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 	
-	@Column(name = "observacao", nullable = false)
-	private Integer descricao;
-	
-	@JsonIgnore
-	@OneToMany(mappedBy = "cozinha")
-	private List<Restaurante> restaurantes = new ArrayList<>();
-	
-	
-	
-
 }
