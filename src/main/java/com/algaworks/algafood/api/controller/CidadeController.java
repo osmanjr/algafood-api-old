652ed0c49f38fc06e386169974f1c5ff4985b97e
osman.siqueira <osman.siqueira@gmail.com>
2023-06-02 17:15:31 -0300

    [commit 048] - Codigo da Aula 8.13: Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -75,28 +75,6 @@ public class CidadeController {
 		cadastroCidade.excluir(cidadeId);	
 	}
 
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e ) {
-		Problema problema = Problema.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(problema);
-	}
-	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e ) {
-
-		Problema problema = Problema.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(problema);
-	}
 
 
 }
