6564a4b5e736581d9e3602a80c58068bc64cabb5
osman.siqueira <osman.siqueira@gmail.com>
2023-06-02 17:27:55 -0300

    [commit 049] - Codigo da Aula 8.14: Desafio: Criando o exceptionHandler para a exceção de objeto em uso.

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHander.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHander.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
@@ -25,6 +26,17 @@ public class ApiExceptionHander {
 				.body(problema);
 	}
 	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e ) {
+		Problema problema = Problema.builder()
+				.datahora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT)
+				.body(problema);
+	}
+
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<?> tratarNegocioException(NegocioException e ) {
 
